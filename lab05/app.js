e7aa8738d1f19e0f28427b8c3b55d239f3e27638
yorn-panha <985112@CS.MUM.EDU>
2016-10-07 14:21:05 -0500

    second commit

--- lab05/app.js
+++ lab05/app.js
@@ -3,7 +3,9 @@ var app = express();
 var inventors = require("./inventors.json");
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.set('x-powered-by', false);
+app.set('strict routing', true);
+app.enable('case sensitive routing');
 app.get('/inventors', function (req, res) {
   res.render('index', {inventors: inventors});
 });
